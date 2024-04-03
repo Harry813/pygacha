f280faa4a5af44c55264d9023284073566f8f715
harry.wang <harry.wang@williamoneilchina.com>
2024-04-03 17:08:44 +0800

    Finish Simple

--- gacha/simple.py
+++ gacha/simple.py
@@ -1,7 +1,7 @@
 import random
 
 from gacha import GachaPoolBase
-from gacha.base import GachaRarityBase
+from gacha.base import GachaRarityBase, GachaItem
 
 
 class SimpleRarity(GachaRarityBase):
@@ -10,14 +10,51 @@ class SimpleRarity(GachaRarityBase):
         self.base_weight = base_weight
 
 
-class SimpleGacha(GachaPoolBase):
-    def _draw(self):
-        total_weight = sum(item.adjusted_weight for item in self.items)
-        rand_val = random.uniform(0, total_weight)
+class SimpleItem(GachaItem):
+    def __init__(self, name, rarity: SimpleRarity, inst=None, adjusted_modifier=0):
+        super().__init__(name, rarity, inst, adjusted_modifier)
 
-        current_sum = 0
-        for item in self.items:
-            current_sum += item.adjusted_weight
-            if rand_val <= current_sum:
+
+class SimpleGacha(GachaPoolBase):
+    def _draw_rarity(self):
+        total_weight = sum(rarity.base_weight for rarity in self.rarity_map.keys())
+        print(total_weight)
+        pick = random.uniform(0, total_weight)
+        current = 0
+        for rarity in self.rarity_map.keys():
+            current += rarity.base_weight
+            if current >= pick:
+                return rarity
+
+    def _draw_item(self, rarity):
+        items_of_rarity = [item for item in self.rarity_map[rarity]]
+        total_modifier = sum(item.adjusted_weight for item in items_of_rarity)
+        pick = random.uniform(0, total_modifier)
+        current = 0
+        for item in items_of_rarity:
+            current += item.adjusted_weight
+            if current >= pick:
                 return item
-        return None
+
+    def pre_draw(self):
+        for rarity, items in self.rarity_map.items():
+            rest_weight = 1
+            if sum([i.adjusted_modifier for i in items]) > 1:
+                raise ValueError("Total adjusted modifier exceeds 1")
+
+            modified_items = [i for i in items if i.adjusted_modifier > 0]
+            not_modified_items = [i for i in items if i.adjusted_modifier == 0]
+
+            for i in modified_items:
+                i.adjusted_weight = i.adjusted_modifier
+                rest_weight -= i.adjusted_modifier
+
+            for i in not_modified_items:
+                i.adjusted_weight = rest_weight / len(not_modified_items)
+
+    def _draw(self):
+        rarity = self._draw_rarity()
+        print(rarity, end=" ")
+        item = self._draw_item(rarity)
+        print(item)
+        return item
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 from gacha import SimpleRarity, SimpleGacha
 from gacha.base import GachaItem
+from gacha.simple import SimpleItem
 
 # 定义稀有度
 common = SimpleRarity("Common", 0.5)
@@ -9,16 +10,17 @@ rare = SimpleRarity("Rare", 0.3)
 epic = SimpleRarity("Epic", 0.15)
 legendary = SimpleRarity("Legendary", 0.05)
 
-sword_c = GachaItem("C Sword", common, "Sword", adjusted_modifier=0.8)
-shield_c = GachaItem("C Shield", common, "Shield")
-spear_r = GachaItem("R Spear", rare, "Spear")
-armor_r = GachaItem("R Armor", rare, "Armor")
-staff_e = GachaItem("E Magic Staff", epic, "Staff")
-dragon_l = GachaItem("L Dragon", legendary, "Dragon")
+sword_c = SimpleItem("C Sword", common, "Sword", adjusted_modifier=0.8)
+shield_c = SimpleItem("C Shield", common, "Shield")
+spear_r = SimpleItem("R Spear", rare, "Spear")
+armor_r = SimpleItem("R Armor", rare, "Armor")
+staff_e = SimpleItem("E Magic Staff", epic, "Staff")
+dragon_l = SimpleItem("L Dragon", legendary, "Dragon")
 
 # 创建SimpleGacha实例并添加项目
 gacha = SimpleGacha()
 gacha.add_item(sword_c, shield_c, spear_r, armor_r, staff_e, dragon_l)
+gacha.show_items()
 
 # 进行多次抽取并记录结果
 statics = {}
@@ -29,7 +31,7 @@ for r in results:
     else:
         statics[r['item'].name] += 1
 
-# 绘制饼图展示结果
+# 绘制饼图展示结果Q
 plt.figure(figsize=(10, 8))
 plt.pie(statics.values(), labels=statics.keys(), autopct='%1.1f%%')
 plt.title("Gacha Draw Results Distribution")
