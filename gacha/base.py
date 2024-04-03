a63cc618d1e05d9272a606038df7fcdba6ac1bc7
harry.wang <harry.wang@williamoneilchina.com>
2024-04-03 17:09:06 +0800

    Update

--- gacha/base.py
+++ gacha/base.py
@@ -4,9 +4,10 @@ from abc import ABC, abstractmethod
 class GachaItem:
     def __init__(self, name, rarity: "GachaRarityBase", inst=None, adjusted_modifier=0):
         """
-        :param name:
-        :param rarity: 稀有度
-        :param inst:
+        :param name: 稀有度名称
+        :param rarity: 稀有度类型
+        :param inst: Item实例
+        :param adjusted_modifier: 在总的稀有度中的比例。0表示不参与调整
         """
         self.name = name
         self.rarity = rarity
@@ -31,48 +32,31 @@ class GachaPoolBase(ABC):
     def __init__(self):
         self.items = []
         self.draw_count = 0
+        self.rarity_map = {}
 
-    def _adjust_weights(self):
-        rarity_modifier_totals = {}
-        rarity_modifier_count = {}
-        rarity_counts = {}
-
-        # 计算每个稀有度下的物品数量及modifier总和
-        for item in self.items:
-            rarity_name = item.rarity.name
-            if rarity_name not in rarity_modifier_totals:
-                rarity_modifier_totals[rarity_name] = item.adjusted_modifier
-                rarity_modifier_count[rarity_name] = 0
-                rarity_counts[rarity_name] = 1
-            else:
-                rarity_modifier_totals[rarity_name] += item.adjusted_modifier
-                rarity_counts[rarity_name] += 1
-
-            if item.adjusted_modifier > 0:
-                rarity_modifier_count[rarity_name] += 1
-
-        print(rarity_modifier_totals, rarity_counts)
-        for item in self.items:
-            rarity_name = item.rarity.name
-            if item.adjusted_modifier > 0:
-                item.adjusted_weight = (item.rarity.base_weight * item.adjusted_modifier) / rarity_modifier_count[
-                    rarity_name]
-            else:
-                remaining_weight = (1 - rarity_modifier_totals[rarity_name]) * item.rarity.base_weight
-                item.adjusted_weight = remaining_weight / (rarity_counts[rarity_name] - rarity_modifier_count[rarity_name])
-
-            print(str(item), item.adjusted_weight)
+    def show_items(self):
+        for k, v in self.rarity_map.items():
+            print(k)
+            for i in v:
+                print("-", i)
 
     def add_item(self, *item):
         for i in item:
             self.items.append(i)
+            if i.rarity not in self.rarity_map:
+                self.rarity_map[i.rarity] = []
+            self.rarity_map[i.rarity].append(i)
 
     @abstractmethod
     def _draw(self):
         pass
 
+    def pre_draw(self):
+        pass
+
     def draw(self, start_draw_count: int = 0, draw_count: int = 1):
-        self._adjust_weights()
+        self.pre_draw()
+
         self.draw_count = start_draw_count
         results = []
         for _ in range(draw_count):
